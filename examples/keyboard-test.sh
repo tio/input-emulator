c09d18a956235212ae5d7a17d175649f5e91b84e
Martin Lund <martin.lund@keep-it-simple.com>
2023-01-22 18:07:14 +0100

    Update keyboard example

--- examples/keyboard-test.sh
+++ examples/keyboard-test.sh
@@ -1,7 +1,19 @@
 #! /bin/bash
 
+# Simple keyboard test
+
 ie=input-emulator
 
 ${ie} start kbd
-${ie} kbd type "Hello World"
+
+# Say hello
+${ie} kbd type "echo 'Hello World!'"
+sleep 0.5
+${ie} kbd key enter
+
+# Press ctrl-l to clear screen
+${ie} kbd keydown ctrl
+${ie} kbd key l
+${ie} kbd keyup ctrl
+
 ${ie} stop kbd
