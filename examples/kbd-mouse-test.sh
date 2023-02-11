8e3a2fedb068dd9f8d10f3f604ca5c078def8755
Martin Lund <martin.lund@prevas.dk>
2023-02-11 19:52:47 +0100

    Update examples

--- examples/kbd-mouse-test.sh
+++ examples/kbd-mouse-test.sh
@@ -7,7 +7,7 @@ ie=input-emulator
 ${ie} start mouse --x-max 2560 --y-max 1440
 ${ie} start kbd
 ${ie} status
-${ie} mouse click left
+${ie} mouse button left
 ${ie} mouse move -10000 -10000
 ${ie} mouse move 1280 720
 ${ie} kbd type 'Hello World'
