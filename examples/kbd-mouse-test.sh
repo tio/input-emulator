334d08f90f733a332de14a527ddfdaad0a134f79
Martin Lund <martin.lund@keep-it-simple.com>
2023-03-08 23:35:17 +0100

    Update examples

--- examples/kbd-mouse-test.sh
+++ examples/kbd-mouse-test.sh
@@ -4,11 +4,12 @@
 
 ie=input-emulator
 
-${ie} start mouse --x-max 2560 --y-max 1440
+${ie} start mouse --x-max 1920 --y-max 1080
 ${ie} start kbd
 ${ie} status
 ${ie} mouse button left
 ${ie} mouse move -10000 -10000
-${ie} mouse move 1280 720
-${ie} kbd type 'Hello World'
+${ie} mouse move 960 540
+${ie} kbd type "echo 'Hello World'"
+${ie} kbd key enter
 ${ie} stop all
